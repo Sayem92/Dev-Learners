8a93a7b52a700913a569dd7dd37a06553ce0f144
Sayem92 <sayemmiha123@gmail.com>
2022-10-11 01:04:01 +0600

    add chart

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Statistics = () => {
+    const data = useLoaderData().data;
+    // console.log(data);
     return (
         <div>
-            <h1>this is statistics</h1>
+           <div style={{width:"100%", height: "400px", padding:"20px 30px 20px 5px"}}>
+            <ResponsiveContainer width="100%" height="100%">
+            <LineChart width={600} height={300} data={data}>
+            <Line type="monotone" dataKey="total" stroke="#8884d8" />
+            <CartesianGrid stroke="#ccc"  strokeDasharray="5 5"/>
+            <XAxis dataKey="name" />
+            <YAxis />
+           <Tooltip />
+            </LineChart>
+            </ResponsiveContainer>
+        </div>
         </div>
     );
 };
--- src/utilities/Routes.jsx
+++ src/utilities/Routes.jsx
@@ -26,6 +26,7 @@ export const routes = createBrowserRouter([
             },
             {
                 path:'/statistics',
+                loader:() =>fetch('https://openapi.programming-hero.com/api/quiz'),
                 element: <Statistics></Statistics>
             },
             {
