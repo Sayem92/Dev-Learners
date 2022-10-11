b10fda6a31b6275ebd6831ca95622bdedebdffb6
Sayem92 <sayemmiha123@gmail.com>
2022-10-11 15:30:54 +0600

    add sweet alert

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+
 import Questions from './Questions';
 
 const Quiz = () => {
@@ -9,14 +10,11 @@ const Quiz = () => {
     // console.log(questionsArray);
 
 
-    // const ansEye =(correctAnswer)=>{
-       
-    // console.log(correctAnswer);
-    // }
-
     return (
         <div>
+            
             <section className="">
+           
                 <div className="container flex flex-col justify-center p-4 mx-auto md:p-8">
                     <p className="p-2 text-4xl font-medium tracking-wider text-center text-blue-600 mb-4">Quiz for {quizData.name}</p>
                     {
